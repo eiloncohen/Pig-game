d93725278133993635052e0195c1c1ea07800fba
Eilon Cohen <eiloncohen20@gmail.com>
2019-12-03 15:06:49 +0200

    css + html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-# GAME RULES:
+/*
+GAME RULES:
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
@@ -6,4 +7,4 @@
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
- 
+*/
