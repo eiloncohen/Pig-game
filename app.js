e1915b7dd58bf159bb6d087c4103b48f8a1111ff
Eilon Cohen <eiloncohen20@gmail.com>
2019-12-06 17:59:34 +0200

    Last updates and finish, game ready!

--- app.js
+++ app.js
@@ -9,26 +9,80 @@ GAME RULES:
 
 */
 
+
+
+
+
+// variables
+
 let scores , roundScore , activePlayer, dice;
 
 scores = [0,0];  // array that store the scores of both players
 roundScore = 0;
 activePlayer = 0;
+let gamePlaying = true;
+
+
+
+
+// events
 
 
 document.querySelector('.btn-new').addEventListener('click', function(){
-document.getElementById('score-0').textContent = '0'; 
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.querySelector(`.player-0-panel`).classList.remove("winner");
-document.querySelector(`.player-1-panel`).classList.remove("winner");
-document.querySelector('.dice').style.display = 'none';
+    InitializationGame();
+});
+
+
+document.querySelector('.btn-roll').addEventListener('click' , function(){  // display the correct photo for roll dice 
+    if(gamePlaying)
+    {
+    let randNum = Math.floor(Math.random()*6 ) + 1;  
+    let dice = document.querySelector('.dice');
+    dice.style.display = 'block';
+    dice.src = 'dice-' + randNum + '.png';
+    let pic = `dice-${randNum}.png`;
+    UpdateCurrentBox(pic); // the num enter to the right place.
+    }
 
 });
 
 
-function WhichNum(SrcOfDice)
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    if(gamePlaying){
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
+    ifWin();
+    roundScore = 0;
+    document.getElementById(`current-${activePlayer}`).textContent = roundScore; // finish to update first player
+    ChangePlayer();
+    }
+    });
+
+
+
+
+
+// functions
+
+function InitializationGame()  // Initialize the game
+{
+    document.getElementById('score-0').textContent = '0'; 
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector(`.player-0-panel`).classList.remove("winner");
+    document.querySelector(`.player-1-panel`).classList.remove("winner");
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('name-0').textContent = 'PLAYER 1';
+    document.getElementById('name-1').textContent = 'PLAYER 2';
+    scores[0] = 0;
+    scores[1] = 0;
+
+}
+
+
+function UpdateCurrentBox(SrcOfDice)  // add the random number from dice to the roundScore variable and display it on the current box round
 {
     let randNum;
     switch(true){
@@ -60,17 +114,18 @@ function WhichNum(SrcOfDice)
     }
 }
 
-function ifWin()
+function ifWin() // check if one of the player won the game
 {
-    if(scores[activePlayer] > 100)
+    if(scores[activePlayer] >= 100)
     {
         document.querySelector(`.player-${activePlayer}-panel`).classList.add("winner");
         document.getElementById(`name-${activePlayer}`).textContent = 'WINNER!';
-        alert(`game over! player number ${activePlayer} won! `);
+        alert(`game over! player number ${activePlayer+1} won! `);
+        gamePlaying = false;
     }
 }
 
-function ChangePlayer()
+function ChangePlayer()  // switch the turn's 
 {
     document.querySelector(`.player-${activePlayer}-panel`).classList.remove("active");
     switch(true)
@@ -86,23 +141,7 @@ function ChangePlayer()
     document.querySelector('.dice').style.display = 'none';
 }
 
-document.querySelector('.btn-roll').addEventListener('click' , function(){  // display the correct photo for roll dice 
-    let randNum = Math.floor(Math.random()*6 ) + 1;  
-    let dice = document.querySelector('.dice');
-    dice.style.display = 'block';
-    dice.src = 'dice-' + randNum + '.png';
-    let pic = `dice-${randNum}.png`;
-    WhichNum(pic); // the num enter to the right place.
-
-});
 
 
- document.querySelector('.btn-hold').addEventListener('click',function(){
- scores[activePlayer] = scores[activePlayer] + roundScore;
- document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
- ifWin();
- roundScore = 0;
- document.getElementById(`current-${activePlayer}`).textContent = roundScore; // finish to update first player
- ChangePlayer();
- });
+
 
